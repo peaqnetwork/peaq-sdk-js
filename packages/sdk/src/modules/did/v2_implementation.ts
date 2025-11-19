e1b146e0e0c9185de609380172a23f29e1979550
jpgundrum <jgundrum3@gmail.com>
2025-11-19 12:50:25 -0600

    fix: address logic for did removal

--- packages/sdk/src/modules/did/v2_implementation.ts
+++ packages/sdk/src/modules/did/v2_implementation.ts
@@ -47,6 +47,8 @@ export class DIDV2Implementation extends Base {
     statusCallback?: (result: TransactionStatusCallback) => void | Promise<void>,
     txOptions?: txOptions
   ): Promise<DidWriteResult> {
+    // TODO: rename didAddress to subjectAddress
+    // 12 char maximum length for projectId (for evm) and then 6 char max for ss58
     const { name, controller, didAddress, verificationMethods = [], services = [], signature } = options;
 
     // Get the connected wallet/keypair address
@@ -177,9 +179,9 @@ export class DIDV2Implementation extends Base {
     const { name, address } = options;
 
     if (this.metadata.chainType === ChainType.EVM) {
-      return this._removeEvm(name, (this.metadata.pair as any)?.address || address, statusCallback, txOptions);
+      return this._removeEvm(name, address || (this.metadata.pair as any)?.address, statusCallback, txOptions);
     }
-    return this._removeSubstrate(name, (this.metadata.pair as any)?.address || address, statusCallback);
+    return this._removeSubstrate(name, address || (this.metadata.pair as any)?.address, statusCallback);
   }
 
   // ---------------------------------------------------------
